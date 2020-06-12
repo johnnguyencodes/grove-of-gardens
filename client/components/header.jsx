f925793cb97714b40b7aa2607d23eaf385ba3916
John Nguyen <jnguyen92780@gmail.com>
2020-06-12 11:46:43 -0700

    defined App class component in client/component/app.jsx that is stateless.  Defined header component in client/component/header.jsx that renders a simple header with a logo.

---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="header-container bg-dark">
+
+      <h1>Sales</h1>
+      <img src="../../server/public/images/wicked-logo.png" alt="logo"/>
+    </div>
+  );
+}
