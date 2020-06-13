c44ce56e9ae630550941ab8951e691b26819b3ab
John Nguyen <jnguyen92780@gmail.com>
2020-06-13 15:22:23 -0700

    modified how the bootstrap card will be returned from ProductListItem so that the entire card can be clicked using the .stretched-link class.  Then, completed the onClick for setView so the arguments passed in will be the product's name and the product's productId

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,19 +4,16 @@ export default function ProductListItem(props) {
   const product = props.product;
   const pricing = product.price;
   const pricingFormatter = price => (price / 100).toFixed(2);
-  const setView = props.setView;
   return (
-    <button className="my-4 pb-3 col-4" id={product.productId} onClick={setView}>
-      <div className="my-4 pb-3 col-4" id={product.productId}>
-        <div className="card h-100">
-          <img src={product.image} className="card-img-top" alt={product.name} />
-          <div className="card-body h-100">
-            <h5 className="card-title text-left">{product.name}</h5>
-            <p className="card-price text-secondary font-weight-bold text-left">${pricingFormatter(pricing)}</p>
-            <p className="card-text text-left">{product.shortDescription}</p>
-          </div>
+    <a className="my-4 pb-3 col-4" id={product.productId}>
+      <div className="card h-100">
+        <img src={product.image} className="card-img-top" alt={product.name} />
+        <div className="card-body h-100 stretched-link" onClick={() => props.setView(product.name, { productId: product.productId })}>
+          <h5 className="card-title text-left">{product.name}</h5>
+          <p className="card-price text-secondary font-weight-bold text-left">${pricingFormatter(pricing)}</p>
+          <p className="card-text text-left">{product.shortDescription}</p>
         </div>
       </div>
-    </button>
+    </a>
   );
 }
