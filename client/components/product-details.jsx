345d82e064b3cc26853c86acaaa01e0a0f602e27
John Nguyen <jnguyen92780@gmail.com>
2020-06-14 10:02:25 -0700

    product-details.jsx state.product is set to an initial value of null, render method will now correctly conditionally render.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -33,38 +33,35 @@ export default class ProductDetails extends React.Component {
   }
 
   render() {
+    if (this.state.product === null) {
+      return null;
+    }
     const productDetails = this.state.product;
-    if (this.state.product) {
-      const pricing = productDetails.price;
-      const pricingFormatter = price => (price / 100).toFixed(2);
-      return (
-        <div>
-          <div className="product-detail-container d-flex justify-content-center col-10 offset-1">
-            <div className="my-4" id={productDetails.productId}>
-              <div className="card">
-                <div className="card-body">
-                  <a className="my-3" onClick={() => this.props.setView('catalog', {})}>Back to Catalog</a>
-                  <div className="col-12 my-4 row">
-                    <div className="col-4 m-0 p-0">
-                      <img src={productDetails.image} className="card-img-top" alt={productDetails.name} />
-                    </div>
-                    <div className="col-8">
-                      <h1 className="card-title text-left">{productDetails.name}</h1>
-                      <h4 className="card-price text-secondary font-weight-bold text-left">${pricingFormatter(pricing)}</h4>
-                      <p className="card-text text-left">{productDetails.shortDescription}</p>
-                    </div>
+    const pricing = productDetails.price;
+    const pricingFormatter = price => (price / 100).toFixed(2);
+    return (
+      <div>
+        <div className="product-detail-container d-flex justify-content-center col-10 offset-1">
+          <div className="my-4" id={productDetails.productId}>
+            <div className="card">
+              <div className="card-body">
+                <a className="my-3" onClick={() => this.props.setView('catalog', {})}>Back to Catalog</a>
+                <div className="col-12 my-4 row">
+                  <div className="col-4 m-0 p-0">
+                    <img src={productDetails.image} className="card-img-top" alt={productDetails.name} />
                   </div>
-                  <p className="card-text text-left">{productDetails.longDescription}</p>
+                  <div className="col-8">
+                    <h1 className="card-title text-left">{productDetails.name}</h1>
+                    <h4 className="card-price text-secondary font-weight-bold text-left">${pricingFormatter(pricing)}</h4>
+                    <p className="card-text text-left">{productDetails.shortDescription}</p>
+                  </div>
                 </div>
+                <p className="card-text text-left">{productDetails.longDescription}</p>
               </div>
             </div>
           </div>
         </div>
-      );
-    } else {
-      return (
-        <p>Product details loading...</p>
-      );
-    }
+      </div>
+    );
   }
 }
