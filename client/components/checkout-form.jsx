98a8b7c55c97b4a6be391ce749bc5358392d2fda
John Nguyen <jnguyen92780@gmail.com>
2020-06-15 16:08:28 -0700

    defined  state, handleChange and handleSubmit methods for CheckoutForm

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -1,8 +1,34 @@
-// import React from 'react';
+import React from 'react';
 
-// export default class CheckoutForm extends React.Component {
-//   constructor(props) {
-//     super(props);
+export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-//   }
-// }
+  handleChange(event) {
+    const change = {};
+    change[event.target.name] = event.target.value;
+    this.setState(change);
+  }
+
+  handleSubmit(event) {
+    const newCustomerInfo = {
+      name: this.state.name,
+      course: this.state.creditCard,
+      shippingAddress: this.state.shippingAddress
+    };
+    this.props.onSubmit(newCustomerInfo);
+    this.setState({
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    });
+  }
+}
