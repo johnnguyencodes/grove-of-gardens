d2cf0b87be98925a2ac4be8634a58c52a6d00519
John Nguyen <jnguyen92780@gmail.com>
2020-07-16 14:01:18 -0700

    updating-database

--- server/index.js
+++ server/index.js
@@ -22,12 +22,12 @@ app.get('/api/health-check', (req, res, next) => {
 
 app.get('/api/products', (req, res, next) => {
   const sql = `
-  select "productId",
-         "name",
-         "price",
-         "image",
-         "shortDescription"
-    from "products"
+      select "productId",
+             "name",
+             "price",
+             "image",
+             "shortDescription"
+        from "products"
     order by "name"
   `;
   db.query(sql)
