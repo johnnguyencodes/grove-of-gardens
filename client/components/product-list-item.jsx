f93767698af4553d8ecc4ed94962b17b57ddebc1
John Nguyen <jnguyen92780@gmail.com>
2020-07-17 17:34:39 -0700

    WIP

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -7,11 +7,11 @@ export default function ProductListItem(props) {
   return (
     <div className="my-4 pb-3 col-4 fade-in slide-in" id={product.productId}>
       <div className="card h-100 stretched-link card-product-list-item shadow-sm" onClick={() => props.setView('details', { productId: product.productId })}>
-        <img src={product.image} className="card-img-top" alt={product.name} />
+        <img src={product.image} className="card-img-top mt-3" alt={product.name} />
         <div className="card-body h-100">
           <h5 className="card-title text-left">{product.name}</h5>
-          <p className="card-price text-secondary font-weight-bold text-left">${pricingFormatter(pricing)}</p>
-          <p className="card-text text-left">{product.shortDescription}</p>
+          <p className="card-price text-secondary font-weight-bold text-left">{product.shortDescription}</p>
+          <p className="card-text text-left">Asking Price: ${pricingFormatter(pricing)}</p>
         </div>
       </div>
     </div>
