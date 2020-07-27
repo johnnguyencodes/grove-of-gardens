15afaa2bca18966ef05d08571a590a41c62f14b4
John Nguyen <jnguyen92780@gmail.com>
2020-07-27 15:54:59 -0700

    WIP

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(props) {
   return (
     <footer className="footer-container col-12 d-flex flex-column justify-content-center align-items-center">
       <h1 className="text-light p-0 m-0"><i className="fa fa-gamepad text-light mr-2" aria-hidden="true"></i>Lost Levels Collectibles</h1>
-      <p className="text-light m-0">&#169; 2020 NGUYENing!</p>
+      <p className="text-light m-0">&#169; 2020 Nguyen Brands</p>
     </footer>
   );
 }
