4b508b3380228dca8cbfee28491298277aed6094
John Nguyen <jnguyen92780@gmail.com>
2020-09-11 18:18:32 -0700

    finished styling quantity drop down menu

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -6,7 +6,7 @@ export default class ProductDetails extends React.Component {
     super(props);
     this.state = {
       product: null,
-      quantity: 0,
+      quantity: 1,
       dropdownOpen: false
     };
     this.getProductDetails = this.getProductDetails.bind(this);
@@ -48,14 +48,14 @@ export default class ProductDetails extends React.Component {
     const productDetails = this.state.product;
     const pricing = productDetails.price;
     const productId = productDetails.productId;
-    // const dropdownOpen = this.state.dropdownOpen;
+    const dropdownOpen = this.state.dropdownOpen;
     const pricingFormatter = price => (price / 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
     return (
       <div>
         <div className="product-detail-container d-flex justify-content-center col-8 offset-2">
           <div className="my-4" id={productDetails.productId}>
             <div className="card card-product-details">
-              <div className="card-body">
+              <div className="card-body" onClick={() => this.toggleDropdown()}>
                 <p id="view" onClick={() => this.props.setView('catalog', {})}><u>Back to Catalog</u></p>
                 <div className="col-12 my-4 row">
                   <div className="col-6 m-0 p-0">
@@ -65,24 +65,39 @@ export default class ProductDetails extends React.Component {
                   <div className="col-6">
                     <h1 className="card-title text-left mb-4">{productDetails.name}</h1>
                     <h4 className="card-price text-left mb-4">{productDetails.shortDescription}</h4>
-                    <h5 className="card-text text-left mb-4 text-secondary font-weight-bold">Asking Price: ${pricingFormatter(pricing)}</h5>
+                    <h5 className="card-text text-left mb-4 text-secondary font-weight-bold">
+                      Asking Price: ${pricingFormatter(pricing)}</h5>
                     <p className="card-text text-left mb-4">{productDetails.longDescription}</p>
-                    <div className="row d-flex justify-content-between">
-                      <div className="quantity-dropdown-wrapper">
+                    <div className="d-flex justify-content-between col-6">
+                      <div className="quantity-dropdown-wrapper mt-2">
                         <div className="quantity-dropdown-header">
-                          <div className="quantity-dropdown-header-title"><button type="button" onClick={() => this.toggleDropdown()} className="btn btn-danger col-12">Quantity <i className="fas fa-chevron-down text-white fa-xs"></i></button></div>
+                          <div className="quantity-dropdown-header-title">
+                            <a onClick={() => this.toggleDropdown()}
+                              className={dropdownOpen
+                                ? 'bg-danger text-white border rounded-top px-3 py-2'
+                                : 'bg-danger text-white border rounded px-3 py-2'
+                              }>Qty: {this.state.quantity}
+                              {dropdownOpen
+                                ? <i className="fas fa-chevron-up text-white fa-xs pl-2"></i>
+                                : <i className="fas fa-chevron-down text-white fa-xs pl-2"></i>
+                              }
+                            </a></div>
+
                         </div>
-                        <ul className="quantity-dropdown-list">
-                          <li className="quantity-dropdown-list-item">1</li>
-                          <li className="quantity-dropdown-list-item">2</li>
-                          <li className="quantity-dropdown-list-item">3</li>
-                          <li className="quantity-dropdown-list-item">4</li>
-                          <li className="quantity-dropdown-list-item">5</li>
-                          <li className="quantity-dropdown-list-item">6</li>
-                          <li className="quantity-dropdown-list-item">7</li>
-                          <li className="quantity-dropdown-list-item">8</li>
-                          <li className="quantity-dropdown-list-item">9</li>
-                          <li className="quantity-dropdown-list-item">10</li>
+                        <ul className={dropdownOpen
+                          ? 'quantity-dropdown-list border border-danger rounded-bottom p-0'
+                          : 'quantity-dropdown-list d-none'
+                        }>
+                          <li className="quantity-dropdown-list-item pl-5">1</li>
+                          <li className="quantity-dropdown-list-item pl-5">2</li>
+                          <li className="quantity-dropdown-list-item pl-5">3</li>
+                          <li className="quantity-dropdown-list-item pl-5">4</li>
+                          <li className="quantity-dropdown-list-item pl-5">5</li>
+                          <li className="quantity-dropdown-list-item pl-5">6</li>
+                          <li className="quantity-dropdown-list-item pl-5">7</li>
+                          <li className="quantity-dropdown-list-item pl-5">8</li>
+                          <li className="quantity-dropdown-list-item pl-5">9</li>
+                          <li className="quantity-dropdown-list-item pl-5">10</li>
                         </ul>
                       </div>
                       <button onClick={() => this.props.addToCart(productId)} className="btn text-white col-6">Add to Cart</button>
