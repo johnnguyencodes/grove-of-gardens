a71d78ad719d6a49dbab85921d57c4556b51e1ff
John Nguyen <jnguyen92780@gmail.com>
2020-09-24 13:06:13 -0700

    restyled header and footer

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function Footer(props) {
   return (
-    <footer className="footer-container col-12 d-flex flex-column justify-content-center align-items-center">
-      <h1 className="text-light p-0 m-0"><i className="fa fa-gamepad text-light mr-2" aria-hidden="true"></i>Lost Levels Collectibles</h1>
-      <p className="text-light m-0">&#169; 2020 Nguyen Brands</p>
+    <footer className="footer-container bg-white col-12 d-flex flex-column justify-content-center align-items-center">
+      <img className="footer-logo" src="..\images\grove-of-gardens-logo.jpg" alt="The Gardens of Grove Logo" onClick={() => props.setView('catalog', {})} />
     </footer>
   );
 }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 export default function Header(props) {
   const cartItemCount = props.cartItemCount;
-  let itemSinglePlural;
-  if (cartItemCount === 1) {
-    itemSinglePlural = 'Item';
-  } else {
-    itemSinglePlural = 'Items';
-  }
   return (
-    <header className="header-container col-12 fixed-top">
-      <div className="col-8 offset-2 d-flex justify-content-between align-content-center">
-        <div className="row pl-5">
-          <h1 className="text-light align-text-bottom ml-2 my-1 logo" onClick={() => props.setView('catalog', {})}><i className="fa fa-gamepad text-light mr-2" aria-hidden="true"></i>Lost Levels Collectibles</h1>
+    <header className="header-container bg-white col-12 fixed-top">
+      <div className="col-10 offset-1 d-flex justify-content-between align-content-center">
+        <div className="col-6 pl-5 d-flex justify-content-start">
+          <img className="header-logo" src="..\images\grove-of-gardens-logo.jpg" alt="The Gardens of Grove Logo" onClick={() => props.setView('catalog', {})}/>
         </div>
-        <div className="row pr-5 align-content-center" id="shopping-cart" onClick={() => props.setView('cart', {})}>
-          <h4 className="text-light align-content-center m-0">{cartItemCount} {itemSinglePlural}</h4><i className="fa fa-shopping-cart fa-2x text-light" aria-hidden="true"></i>
+        <div className="col-6 d-flex flex-column align-items-end justify-content-center">
+          <div className="row" id="shopping-cart" onClick={() => props.setView('cart', {})}>
+            <h4 className="text-light align-content-center m-0 text-dark"><i className="fa fa-shopping-cart text-dark mr-2 mb-0 fa-sm" aria-hidden="true"></i>{cartItemCount}</h4>
+          </div>
+          <div className="row align-content-center">
+            <h5 className="text-light align-content-center m-0 text-dark">Hello</h5>
+          </div>
         </div>
       </div>
     </header>
