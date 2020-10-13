acbad5b5cc0e2245948d7f15349e8366c9aacf3b
John Nguyen <jnguyen92780@gmail.com>
2020-10-13 16:01:01 -0700

    cart-summary.jsx mobile responsiveness complete

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -15,35 +15,42 @@ export default function CartSummaryItem(props) {
   if (quantity > 0) {
     disabled = null;
   } else if (quantity <= 0) { disabled = 'disabled'; }
+  let hrClass = null;
+  if (props.cartArray[props.cartArray.length - 1].cartItemId === key) {
+    hrClass = 'd-none';
+  } else {
+    hrClass = 'mx-3';
+  }
   const pricingFormatter1 = price => (price / 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   const pricingFormatter2 = price => ((price * quantityInCart) / 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   return (
-    <div className="my-4" id={productId} key={key}>
-      <div className="card card-cart-summary">
-        <div className="card-body">
+    <div className="m-3" id={productId} key={key}>
+      <div className="card card-cart-summary border-0">
+        <div className="card-body p-0">
           <div className="col-12 p-0 m-0 row">
-            <div className="col-4 d-flex align-items-center">
+            <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 d-flex align-items-center justify-content-center">
               <img src={image} className="cart-summary-img" alt={name} onClick={() => props.setView('details', { productId: productId })}/>
             </div>
-            <div className="col-8 d-flex flex-column justify-content-start">
-              <h3 className="card-title text-left mb-3">{name}</h3>
-              <p className="card-text text-left">{shortDescription}</p>
-              <p className="card-price text-left mb-3">${pricingFormatter1(pricing)}</p>
-              <div className="input-row d-flex align-items-center">
-                <label htmlFor="quantity" className="mr-2 font-weight-bold mb-0"><h6 className="m-0">Quantity:</h6></label>
-                <div className='cart-quantity-input-container mr-4 p-0 d-flex align-items-center'>
+            <div className="col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 d-flex flex-column justify-content-start">
+              <h3 className="card-title text-left my-3">{name}</h3>
+              <p className="card-text text-left mb-3">{shortDescription}</p>
+              <p className="card-price text-left mb-3">${pricingFormatter1(pricing)} per item</p>
+              <div className="input-row d-flex align-items-center mb-3 flex-wrap">
+                {/* <label htmlFor="quantity" className="mr-2 font-weight-bold my-2"><h6 className="m-0">Quantity:</h6></label> */}
+                <div className='cart-quantity-input-container mr-sm-4 mr-md-4 mr-lg-4 mr-xl-4 mr-2 my-2 p-0 d-flex align-items-center'>
                   <a href="#" className="btn rounded-right quantity-decrement-button" onClick={() => props.decrementCartQuantity(quantityIndex)}><i className="fas fa-minus text-white"></i></a>
                   <input type="number" className="cart-quantity-input border rounded text-center" pattern="[0-9]" min="0" onInput={props.quantityMaxLengthCheck} onKeyDown={props.quantityInputValidation} onChange={props.handleQuantityChange.bind(this, quantityIndex)} maxLength="3" value={quantity} />
                   <a href="#" className="btn rounded-left quantity-increment-button" onClick={() => props.incrementCartQuantity(quantityIndex)}><i className="fas fa-plus text-white"></i></a>
                 </div>
-                <button className="btn text-white mr-4" onClick={() => props.updateCartItemQuantity(key, quantity)} disabled={disabled}>Update</button>
-                <button className="btn text-white" onClick={() => props.removeFromCart(key)}>Delete</button>
+                <button className="btn text-white mr-2 mr-sm-4 mr-md-4 mr-lg-4 mr-xl-4 my-2" onClick={() => props.updateCartItemQuantity(key, quantity)} disabled={disabled}>Update</button>
+                <button className="btn text-white my-2" onClick={() => props.removeFromCart(key)}>Delete</button>
               </div>
-              <h5 className="mt-3">Subtotal: ${pricingFormatter2(pricing)}</h5>
+              <h5 className="mb-3">Subtotal: ${pricingFormatter2(pricing)}</h5>
             </div>
           </div>
         </div>
       </div>
+      <hr className={hrClass}/>
     </div>
   );
 }
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -5,7 +5,7 @@ export default function CartSummary(props) {
   const cartArray = props.cartArray;
   if (!(cartArray.length)) {
     return (
-      <div className="col-10 offset-1 px-5">
+      <div className="col-xl-10 offset-xl-1 px-5">
         <p className="mt-3" id="view" onClick={() => props.setView('catalog', {})}><u>Back to Catalog</u></p>
         <h1 className="mt-3">My Cart</h1>
         <div>
@@ -20,38 +20,43 @@ export default function CartSummary(props) {
     totalPricing += (cartArray[i].price * cartArray[i].quantity);
     totalItems += (cartArray[i].quantity);
   }
-
   const pricingFormatter = totalPricing => (totalPricing / 100).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   return (
     <div>
-      <div className="col-8 offset-2 px-5">
+      <div className="col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12 px-5">
         <p className="mt-3" id="view" onClick={() => props.setView('catalog', {})}><u>Back to Catalog</u></p>
         <h1 className="mt-3">{totalItems} items in your cart</h1>
       </div>
-      <div className="cart-summary-container row col-8 offset-2">
-        <div className="col-8 d-flex flex-column card-deck m-0">
-          {cartArray.map(cartSummaryItem => {
-            return (
-              <CartSummaryItem
-                quantityToUpdateArray={props.quantityToUpdateArray}
-                cartSummaryItem={cartSummaryItem}
-                key={cartSummaryItem.cartItemId}
-                setView={props.setView}
-                removeFromCart={props.removeFromCart}
-                quantityInputValidation={props.quantityInputValidation}
-                handleQuantityChange={props.handleQuantityChange}
-                incrementCartQuantity={props.incrementCartQuantity}
-                decrementCartQuantity={props.decrementCartQuantity}
-                quantityMaxLengthCheck={props.quantityMaxLengthCheck}
-                updateCartItemQuantity={props.updateCartItemQuantity} />
-            );
-          })}
+      <div className="cart-summary-container row col-xl-10 offset-xl-1 col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12 pl-5">
+        <div className="col-xl-8 d-flex flex-column mr-3 ml-0 my-0 p-0">
+          <div className="card card-summary my-4 mx-0 p-0">
+            <div className="card-body m-0 p-0">
+              {cartArray.map(cartSummaryItem => {
+                return (
+                  <CartSummaryItem
+                    quantityToUpdateArray={props.quantityToUpdateArray}
+                    cartSummaryItem={cartSummaryItem}
+                    key={cartSummaryItem.cartItemId}
+                    setView={props.setView}
+                    removeFromCart={props.removeFromCart}
+                    quantityInputValidation={props.quantityInputValidation}
+                    handleQuantityChange={props.handleQuantityChange}
+                    incrementCartQuantity={props.incrementCartQuantity}
+                    decrementCartQuantity={props.decrementCartQuantity}
+                    quantityMaxLengthCheck={props.quantityMaxLengthCheck}
+                    updateCartItemQuantity={props.updateCartItemQuantity}
+                    cartArray={props.cartArray}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
-        <footer className="cart-summary-checkout-container col-4 mt-4">
-          <div className="col-12 p-0 m-0 d-flex justify-content-between align-content-center">
+        <footer className="cart-summary-checkout-container col-xl-3 col-12 mt-xl-4 mb-4 p-0">
+          <div className="col-xl-12 p-0 m-0 d-flex justify-content-between align-content-center">
             <div className="card card-order">
               <div className="card-body">
-                <div className="col-12 row">
+                <div className="col-xl-12 p-0">
                   <h4 className="mb-4">Order Total: ${pricingFormatter(totalPricing)}</h4>
                   <button type="submit" onClick={() => props.setView('checkout', {})} className="btn text-white">Checkout</button>
                 </div>
